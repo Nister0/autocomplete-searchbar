8448ed006f23ade76888b34ca8a538e53fea4d21
Nister0 <benjamin-simon@t-online.de>
2026-01-09 11:43:44 +0100

    renamed main.py to dictionary.py, added string_search.py for search algorithm, added main.py for main logic thats gonna combine all the code from the different files.

---
+++ dictionary.py
@@ -0,0 +1,51 @@
+import os
+
+class dictionary:
+    def __init__(self, file):
+        '''
+        intitialize class, open given file and save it to self.data
+        '''
+        self.data = self.divide_data(self.open_file(file))
+
+        if not self.data:
+            raise Exception("File is empty.")
+
+    def open_file(self, file):
+        dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(dir, file)
+
+        if not os.path.exists(file_path):
+            raise Exception(f"file: '{file}' does not exist") 
+        if not os.path.isfile(file_path):
+            raise Exception(f"there is no file: '{file}' in the path '{file_path}")
+
+        with open(file_path, "r+") as f:
+            data = f.read()
+            
+        return data
+    
+    def divide_data(self, data):
+        divData = []
+        divData.append("")
+        #print(data)
+        j = 0
+        for i in data:
+            if i == " ":
+                divData.append("")
+                j += 1
+            else:
+                divData[j] = divData[j] + i
+
+        while divData and divData[-1] == "":
+            divData.pop(-1)
+            
+        return divData
+
+    def print_data(self):
+        for i in self.data:
+            print(i, end=" ")
+        print()
+
+if __name__ == "__main__":
+    d = dictionary("lexikon.txt")
+    d.print_data()
