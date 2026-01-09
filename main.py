e52deb4b55c3656649b892bb919ef6d29202331c
Nister0 <benjamin-simon@t-online.de>
2026-01-09 11:39:06 +0100

    fixed 'search' class functions, added tests and added print function and empty word removal for the list of words.

--- main.py
+++ main.py
@@ -5,29 +5,47 @@ class search:
         '''
         intitialize class, open given file and save it to self.data
         '''
-        def open_file():
-            if not os.path.exists(file):
-                raise Exception(f"path '{file}' does not exist") 
-            if not os.path.isfile(file):
-                raise Exception(f"there is no such file: '{file}'")
+        self.data = self.divide_data(self.open_file(file))
 
-            dir = os.path.dirname(os.path.abspath(__file__))
-            file_path = os.path.join(dir, file)
+        if not self.data:
+            raise Exception("File is empty.")
 
-            with open(file_path, "r+") as f:
-                self.data = f.read()
-                
-            return 0
-        
-        def divide_data():
-            divData = ()
-            j = 0
-            for i in data:
-                if i == " ":
-                    j += 1
-                else:
-                    divData[j] = divData[i] + i 
-                    j += 1
+    def open_file(self, file):
+        dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(dir, file)
 
-            data = divData
+        if not os.path.exists(file_path):
+            raise Exception(f"file: '{file}' does not exist") 
+        if not os.path.isfile(file_path):
+            raise Exception(f"there is no file: '{file}' in the path '{file_path}")
 
+        with open(file_path, "r+") as f:
+            data = f.read()
+            
+        return data
+    
+    def divide_data(self, data):
+        divData = []
+        divData.append("")
+        #print(data)
+        j = 0
+        for i in data:
+            if i == " ":
+                divData.append("")
+                j += 1
+            else:
+                divData[j] = divData[j] + i
+
+        while divData and divData[-1] == "":
+            divData.pop(-1)
+            
+        return divData
+
+    def print_data(self):
+        for i in self.data:
+            print(i, end=" ")
+        print()
+
+if __name__ == "__main__":
+    s = search("lexikon.txt")
+    s.print_data()
