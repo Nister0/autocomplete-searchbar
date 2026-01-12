4c5d68340193ea509ffa52c2dc165dc8df2061c5
Nister0 <benjamin-simon@t-online.de>
2026-01-12 12:39:35 +0100

    minor changes to documentation and more work on match() function.

--- string_search.py
+++ string_search.py
@@ -9,7 +9,7 @@ class search:
         '''
         sorts data word list based on input, sorted by match % to give the highest matching words.
         '''
-        words : list[str]= self.words 
+        words : list[str] = self.words 
         output : dict[str, float] = {}
 
         for i in words:
@@ -18,15 +18,25 @@ class search:
         return output
 
 
-    def match(self, pattern:str, word:str) -> float:
+    def match(self, pattern: str, word: str) -> float:
         '''
         calculates % of matching letters. if str in input: % < 0 else: % = 0.
+        this could be done easier with with python inbuilt functions.
         '''
+        j = 0
+        for i in range(len(word)):
+            if len(word) < len(pattern):
+                return 0
+            
+            if word[i] == pattern[j]:
+                word = word[0:]
+                j += 1
+                if j == len(pattern):
+                    break
+            
+        return len(word) / len(pattern)
 
-        if len(word) < len(pattern):
-            return 0
-        
-        for i in word: 
 
-
-
+if __name__ == "__main__": 
+    s = search([""])
+    print(s.match("all", "hallo"))
