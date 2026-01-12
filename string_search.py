31a861b6cdd716c103cabe6adf22195c38801dea
Nister0 <benjamin-simon@t-online.de>
2026-01-12 11:07:36 +0100

    added documentation for dictionary.py and moved data file into subfolder 'files'.

--- string_search.py
+++ string_search.py
@@ -1,2 +1,16 @@
 class search:
-    pass
+    def __init__(self, words: list[str]):
+        self.words = words 
+
+    def sort(self, input: str) -> list[str]:
+        '''
+        sorts data word list based on input, sorted by match % to give the highest matching words.
+        '''
+        words = self.words 
+
+        for i in words:
+            pass
+
+    def match(self, input:str, word:str) -> float:
+        pass 
+
