4c9b0109bd15909a135cc2d03381caba1f870571
Nister0 <benjamin-simon@t-online.de>
2026-01-12 11:26:32 +0100

    added more logic and documentation.

--- string_search.py
+++ string_search.py
@@ -1,16 +1,32 @@
 class search:
+    '''
+    handles which words will be suggested
+    '''
     def __init__(self, words: list[str]):
         self.words = words 
 
-    def sort(self, input: str) -> list[str]:
+    def sort(self, input: str) -> dict[str, float]:
         '''
         sorts data word list based on input, sorted by match % to give the highest matching words.
         '''
-        words = self.words 
+        words : list[str]= self.words 
+        output : dict[str, float] = {}
 
         for i in words:
-            pass
+            output[i] = self.match(input, i)
 
-    def match(self, input:str, word:str) -> float:
-        pass 
+        return output
+
+
+    def match(self, pattern:str, word:str) -> float:
+        '''
+        calculates % of matching letters. if str in input: % < 0 else: % = 0.
+        '''
+
+        if len(word) < len(pattern):
+            return 0
+        
+        for i in word: 
+
+
 
