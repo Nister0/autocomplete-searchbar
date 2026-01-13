85894fbef79efc79ca17bca17de94f7e60d373b8
Nister0 <106627897+Nister0@users.noreply.github.com>
2026-01-13 16:05:29 +0100

    added basic logic and functions to file.

--- autocomplete.py
+++ autocomplete.py
@@ -1,8 +1,15 @@
-import string_match as sm
+import string_match
 import dictionary as dict_module
 
 class autcomplete:
-    pass
+    def __init__(self, data_set: str):
+        dm = dict_module.dictionary(data_set)
+        sm = string_match.match(dm.data)
+        word = sm.match()
+
+    def suggestion(self):
+        pass
+
 
 if __name__ == "__main__":
     pass 
