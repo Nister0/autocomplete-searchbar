99114198c92e0ad6b0ccac7b680c87754c10550b
Nister0 <benjamin-simon@t-online.de>
2026-01-14 11:45:40 +0100

    reworked function to use fastforward and fixed bugs.

--- autocomplete.py
+++ autocomplete.py
@@ -2,14 +2,14 @@ import string_match
 import dictionary as dict_module
 
 class autcomplete:
-    def __init__(self, data_set: str):
-        dm = dict_module.dictionary(data_set)
-        sm = string_match.match(dm.data)
-        word = sm.match()
+    def __init__(self, data_set : str):
+        self.dm = dict_module.dictionary(data_set)
+        self.sm = string_match.match()
 
-    def suggestion(self):
-        pass
-
+    def suggest(self, pattern : str):
+       return self.sm.fastforward(pattern, self.dm.data)
 
 if __name__ == "__main__":
-    pass 
+    ac = autcomplete("files/lexikon.txt")
+    for i in ac.suggest("ralle"):
+        print(i)
--- string_match.py
+++ string_match.py
@@ -3,41 +3,47 @@ class match:
     handles which words will be suggested
     '''
 
-    def __init__(self, dictionary: list[str]):
-        self.dictionary : list[str] = dictionary 
-        suggestions : list[str] = self.sort()
+    def __init__(self):
+        self.dictionary = []
 
-    def sort(self, input: str) -> dict[str, float]:
+    def fastforward(self, pattern : str, dictionary: list[str]) -> list[tuple[str, float]]:
+        self.dictionary = dictionary
+        return self.sort(pattern)
+
+    def sort(self, pattern: str) -> dict[str, float]:
         '''
         sorts data word list based on input, sorted by match % to give the highest matching words.
         '''
 
-        output : list[tuple[str, float]] = ()
+        output : list[tuple[str, float]] = []
 
         for i in self.dictionary:
-            output.append((i, self._match(input, i)))
+            output.append((i, self._match(pattern, i)))
 
         return self._mergesort(output)
 
 
-    def _match(self, pattern: str, word: str) -> float:
+    def _match(self, pattern: str, dictionary: str) -> float:
         '''
         calculates % of matching letters. if str in input: % < 0 else: % = 0.
         this could be done easier with with python inbuilt functions.
         '''
 
         j = 0
-        for i in range(len(word)):
-            if len(word) < len(pattern):
+        for i in range(len(dictionary)):
+            if len(dictionary) < len(pattern):
                 return 0
             
-            if word[i] == pattern[j]:
-                word = word[0:]
+            if dictionary[i] != pattern[j]:
+                j == 0
+            else:
                 j += 1
-                if j == len(pattern):
-                    break
+            dictionary[0:]
+            '''
+            fix later not working correctly when pattern len == dict len.
+            '''
             
-        return len(pattern) / len(word)
+        return len(pattern) / len(dictionary)
 
     def _mergesort(self, matched_words : list[tuple[str, float]]) -> list[tuple[str, float]]:
         '''
@@ -46,19 +52,18 @@ class match:
 
         if len(matched_words) <= 1:
             return matched_words
-        xs = ()
-        ys = ()
-        nmw = len(matched_words) - 1
-
-        xs = matched_words[0 : nmw//2]
-        ys = matched_words[nmw//2: nmw]
+        
+        # split in half (use proper midpoint so no element is dropped)
+        mid = len(matched_words) // 2
+        xs = matched_words[:mid]
+        ys = matched_words[mid:]
 
         l = self._mergesort(xs)
         r = self._mergesort(ys)
 
         return self._merge(l, r)
     
-    def _merge(self, l : list[tuple[str, float]], r : list[tuple[str, float]]): 
+    def _merge(self, l : list[tuple[str, float]], r : list[tuple[str, float]]) -> list[tuple[str, float]]: 
         '''
         subfunction for merge sort. merges two lists.
         '''
@@ -70,14 +75,15 @@ class match:
             else:
                 xs.append(r.pop(0))
 
+        # append remaining elements (extend, not append, to avoid nested lists)
         if l:
-            xs.append(l[0:(len(l))])
-        if r:
-            xs.append(r[0:(len(r))])
+            xs.extend(l)
+        elif r:
+            xs.extend(r)
 
         return xs
 
 if __name__ == "__main__": 
-    s = match([""])
-    print(s._match("allo", "hallo")) 
+    s = match()
+    print(s.fastforward("hallo",["halloooo"])) 
 
